fceeba8d74bfeb3d0d5e0f919aa3104109d39689
TrippleA-Ashaba <trippleaunit@hotmail.com>
2023-09-07 11:50:37 +0300

    sum of primes in range

---
+++ project_euler/summation_primes.py
@@ -0,0 +1,31 @@
+"""
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+Find the sum of all the primes below two million.
+"""
+
+from functools import reduce
+
+
+def sieve_of_eratosthenes(limit):
+    is_prime = [True] * (limit + 1)
+    is_prime[0] = is_prime[1] = False
+
+    for num in range(2, int(limit**0.5) + 1):
+        if is_prime[num]:
+            for multiple in range(num * num, limit + 1, num):
+                is_prime[multiple] = False
+
+    return [i for i, prime in enumerate(is_prime) if prime]
+
+
+def sum_of_primes_below_n(n):
+    primes = sieve_of_eratosthenes(n - 1)
+    return sum(primes)
+
+
+# Calculate the sum of all primes below two million
+
+
+if __name__ == "__main__":
+    result = sum_of_primes_below_n(10)
+    print(result)
