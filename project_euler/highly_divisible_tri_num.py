b53226d3a677b2ffeac304d58297d8febccc1ef7
TrippleA-Ashaba <trippleaunit@hotmail.com>
2023-09-17 14:45:29 +0300

    complete

--- project_euler/highly_divisible_tri_num.py
+++ project_euler/highly_divisible_tri_num.py
@@ -7,13 +7,26 @@ def triangular(n):
 
 def factors(n):
     stack = []
-    for i in range(1, n + 1):
+
+    sqrt_n = int(n**0.5) + 1
+
+    for i in range(1, sqrt_n):
         if n % i == 0:
             stack.append(i)
+            stack.append(n // i)
 
+    if sqrt_n * sqrt_n == n:
+        stack.append(sqrt_n)
     return stack
 
 
+def find_triangular_with_more_than_n_factors(limit, facts):
+    for tri_num in triangular(limit):
+        factors_list = factors(tri_num)
+        if len(factors_list) > facts:
+            return tri_num
+
+
 if __name__ == "__main__":
-    for val in triangular(10):
-        print(factors(val))
+    result = find_triangular_with_more_than_n_factors(100000, 500)
+    print(result)
